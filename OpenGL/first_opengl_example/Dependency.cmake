89714a255d3892badb0d8fd4c0f80e9b9afe3453
kjun627 <kjun627@naver.com>
2025-10-09 15:15:15 +0900

    glad setting for mac - self binding

--- OpenGL/first_opengl_example/Dependency.cmake
+++ OpenGL/first_opengl_example/Dependency.cmake
@@ -45,3 +45,21 @@ ExternalProject_Add(
 
 set(DEP_LIST ${DEP_LIST} dep_glfw)
 set(DEP_LIBS ${DEP_LIBS} glfw3)
+
+
+## 윈도우라면 glad 직접 바이딩 하지 않고 아래로 해결 가능
+# # glad
+# ExternalProject_Add(
+#     dep_glad
+#     GIT_REPOSITORY "https://github.com/Dav1dde/glad"
+#     GIT_TAG "v0.1.34"
+#     GIT_SHALLOW 1
+#     UPDATE_COMMAND ""
+#     PATCH_COMMAND ""
+#     CMAKE_ARGS
+#         -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+#         -DGLAD_INSTALL=ON
+#     TEST_COMMAND ""
+#     )
+# set(DEP_LIST ${DEP_LIST} dep_glad)
+# set(DEP_LIBS ${DEP_LIBS} glad)
