b5e035d54ae2bb7d2b03207a0ceda1dbde4eabf5
Junhyung Kim <kjun627@naver.com>
2025-10-18 13:16:53 +0900

    study: transformation on every frame

--- OpenGL/opengl_example/src/context.cpp
+++ OpenGL/opengl_example/src/context.cpp
@@ -123,7 +123,12 @@ bool Context::Init(){
 void Context::Render(){
     glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
-
+    
     m_program->Use();
+    auto projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.01f, 10.0f);
+    auto view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+    auto model = glm::rotate(glm::mat4(1.0f),glm::radians((float)glfwGetTime()*120.0f), glm::vec3(1.0f, 0.5f,0.0f));
+    auto transform = projection * view * model;
+    m_program->SetUniform("transform", transform);
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 }
